b6adde1e1e54d1f9ffca5a78e31429695791183d
Stefania-Larisa Barabas <stefaniabarabas@gmail.com>
2025-03-03 14:43:40 +0100

    Code review fixes

--- host-app/app/page.tsx
+++ host-app/app/page.tsx
@@ -12,7 +12,7 @@ interface RemoteButtonProps {
 
 export default function HomePage() {
   const [RemoteButton, setRemoteButton] = useState<React.ComponentType<RemoteButtonProps> | null>(null);
-  const remoteUrl = process.env.NEXT_REMOTE_MFE_WIDGETS ?? "http://localhost:3005/remoteEntry.js";
+  const remoteUrl = process.env.NEXT_PUBLIC_REMOTE_MFE_WIDGETS ?? "http://localhost:3005/remoteEntry.js";
   useEffect(() => {
     loadRemoteComponent(remoteUrl, 'mfeWidgets', './button')
     .then((module) => setRemoteButton(() => module.default))
--- host-app/utils/loadRemoteComponent.ts
+++ host-app/utils/loadRemoteComponent.ts
@@ -1,3 +1,4 @@
+// TODO: defined ALLOWED_DOMAINS and check inside loadRemoteComponent if the remoteURL is a trusted source
 export async function loadRemoteComponent(remoteUrl: string, scope: string, module: string) {
     return new Promise<any>((resolve, reject) => {
       if (!(window as any)[scope]) {
@@ -27,20 +28,26 @@ export async function loadRemoteComponentStyle<T = any>(
     scope: string,
     module: string
   ): Promise<T> {
-    if (!window[scope]) {
-      const script = document.createElement('script');
-      script.src = remoteUrl;
-      script.async = true;
-      document.body.appendChild(script);
-  
-      await new Promise((resolve) => {
-        script.onload = resolve;
-      });
-    }
-  
-    const factory = await window[scope].get(module);
-    const mod = factory();
-  
-    return mod as T;
+    try {
+        if (!(window as any)[scope]) {
+          const script = document.createElement('script');
+          script.src = remoteUrl;
+          script.async = true;
+          
+          await new Promise<void>((resolve, reject) => {
+            script.onload = () => resolve();
+            script.onerror = () => reject(new Error(`Failed to load style from ${remoteUrl}`));
+            document.body.appendChild(script);
+          });
+        }
+        
+        const factory = await (window as any)[scope].get(module);
+        const mod = factory();
+        
+        return mod as T;
+      } catch (error) {
+        console.error(`Failed to load remote component style from ${remoteUrl}:`, error);
+        throw error;
+      }
   }
   
